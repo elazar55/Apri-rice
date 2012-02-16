32e457ea25220506304d859e65968eca784293f7
unknown <Cyberaction@cyber-FY71QQ1.(none)>
2012-02-15 21:13:28 -0500

    fix shit

--- org/tinkernut/apririce/Command.java
+++
@@ -1,9 +0,0 @@
-package org.tinkernut.apririce;
-
-public interface Command {
-
-    public void exec(final Bot bot, String params);
-
-    public void execPriv(final Bot bot, User sender, String params);
-
-}
