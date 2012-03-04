005e81f63870d2ec5c11271245e23960f5af8d6d
Elazar55 <elazar55@hotmail.com>
2012-03-04 21:24:27 +0000

    Continued DefineCommand().

--- src/org/tinkernut/apririce/commands/DefineCommand.java
+++ src/org/tinkernut/apririce/commands/DefineCommand.java
@@ -13,36 +13,33 @@ import jerklib.events.MessageEvent;
 import org.tinkernut.apririce.Bot;
 import org.tinkernut.apririce.User;
 import org.tinkernut.apririce.commands.Command;
+import org.tinkernut.apririce.textUtils.Parser;
 import org.tinkernut.apririce.textUtils.TextBuffer;
 
 public class DefineCommand extends Command {
-	Map<String, URL> urlMap;
+	Map<String, String> urlMap;
 	URLConnection urlConnection;
 	InputStreamReader inputStream;
 	BufferedReader bReader;
-	
+
 	@Override
 	public void init(final Bot b, final String s, final MessageEvent m) {
 		super.init(b, s, m);
-		urlMap = new HashMap<String, URL>();
-
-		try {
-			urlMap.put("urban", new URL("http://www.urbandictionary.com/define.php?term="));
-		}catch (MalformedURLException e) {
-			new TextBuffer().addAndDisplay("Internal error: Malformed URL", me);
-		}
+		urlMap = new HashMap<String, String>();
+		urlMap.put("urban", "http://www.urbandictionary.com/define.php?term=");
 	}
 
 	public void run() {
-		if (params.equals("urban")) {
+		if (params.contains("urban") && Parser.stripAguments(params) != "") {
 			try {
-				urlConnection = urlMap.get(params).openConnection();
-				inputStream = new InputStreamReader(urlConnection.getInputStream());
-				bReader = new BufferedReader(inputStream);
-				new TextBuffer().addAndDisplay(bReader.readLine(), me);
+				urlConnection = new URL(urlMap.get(params.substring(0, params.indexOf(' ')))+Parser.stripAguments(params)).openConnection();
+				bReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 			} catch (IOException e) {
 				new TextBuffer().addAndDisplay("Unable to establish connection.", me);
 			}
+		}
+		else {
+			new TextBuffer().addAndDisplay("Input something to define", me);
 		}
 	}
 
