5f866b8659363145ddb8334a4b263d353efc15ce
Elazar55 <elazar55@hotmail.com>
2012-03-04 20:53:38 +0000

    Continued DefineCommand().

--- src/org/tinkernut/apririce/commands/DefineCommand.java
+++ src/org/tinkernut/apririce/commands/DefineCommand.java
@@ -1,9 +1,12 @@
 // TODO: Finish DefineCommand
-
 package org.tinkernut.apririce.commands;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import jerklib.events.MessageEvent;
@@ -14,6 +17,10 @@ import org.tinkernut.apririce.textUtils.TextBuffer;
 
 public class DefineCommand extends Command {
 	Map<String, URL> urlMap;
+	URLConnection urlConnection;
+	InputStreamReader inputStream;
+	BufferedReader bReader;
+	
 	@Override
 	public void init(final Bot b, final String s, final MessageEvent m) {
 		super.init(b, s, m);
@@ -28,7 +35,14 @@ public class DefineCommand extends Command {
 
 	public void run() {
 		if (params.equals("urban")) {
-			
+			try {
+				urlConnection = urlMap.get(params).openConnection();
+				inputStream = new InputStreamReader(urlConnection.getInputStream());
+				bReader = new BufferedReader(inputStream);
+				new TextBuffer().addAndDisplay(bReader.readLine(), me);
+			} catch (IOException e) {
+				new TextBuffer().addAndDisplay("Unable to establish connection.", me);
+			}
 		}
 	}
 
