be373b9a33a1bf1e021018b8714a48520d5b350d
Elazar55 <elazar55@hotmail.com>
2012-03-17 16:30:35 +0000

    blah

--- src/org/tinkernut/apririce/commands/HelpCommand.java
+++ src/org/tinkernut/apririce/commands/HelpCommand.java
@@ -1,8 +1,10 @@
 package org.tinkernut.apririce.commands;
 
+import org.tinkernut.apririce.textUtils.TextBuffer;
+
 public class HelpCommand extends Command {
 	public void exec() {
-		System.out.println("This is the public secotr!");
+		TextBuffer.addAndDisplay("I'm sorry Dave. I'm afraid I can't let you do that.", me);
 	}
 
 	public void execPriv() {
