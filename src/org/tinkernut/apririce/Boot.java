813554589819f4ada967d0c8a7fd6570e348d080
Elazar55 <elazar55@hotmail.com>
2012-03-17 15:03:34 +0000

    Comments like a baws.

--- src/org/tinkernut/apririce/Boot.java
+++ src/org/tinkernut/apririce/Boot.java
@@ -2,10 +2,9 @@ package org.tinkernut.apririce;
 
 public class Boot {
 	public static void main(String[] args) {
-		Thread tinkernutThread = new Thread(new Bot("irc.geekshed.net", "#tinkernut", "apri-rice_tinkernut"));
-		Thread tinkernutTestThread = new Thread(new Bot("irc.geekshed.net", "#tinkernut_test_room", "apri-rice_tinkernut-test-room"));
+		//Create thread(s) for bot.
+		Thread tinkernutTestThread = new Thread(new Bot("irc.geekshed.net", "#tinkernut_test_room", "Apri-rice_tinkernut-test-room"));
 		
-		tinkernutThread.start();
 		tinkernutTestThread.start();
 	}
 }
