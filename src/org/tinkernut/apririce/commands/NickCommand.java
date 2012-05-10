32e1c71e1de6fd3cd952fb54a9c94ce22ac12980
Elazar55 <elazar55@hotmail.com>
2012-05-10 20:07:31 +0100

    Nick change command lol.

---
+++ src/org/tinkernut/apririce/commands/NickCommand.java
@@ -0,0 +1,27 @@
+package org.tinkernut.apririce.commands;
+
+import org.tinkernut.apririce.textUtils.Parser;
+import org.tinkernut.apririce.textUtils.TextBuffer;
+
+public class NickCommand extends Command{
+	
+	String helpText = "Do you REALLY need help with this? And no, you can't set the nick as \"help\".";
+
+	@Override
+	void exec() {
+		if (Parser.getFirstArgument(params).equalsIgnoreCase("help")) {
+			TextBuffer.addAndDisplay(helpText, me);
+			return;
+		}
+		me.getSession().changeNick(params);
+	}
+
+	@Override
+	void execPriv() {
+		if (Parser.getFirstArgument(params).equalsIgnoreCase("help")) {
+			me.getSession().sayPrivate(me.getNick(), helpText);
+			return;
+		}
+		me.getSession().changeNick(params);
+	}
+}
