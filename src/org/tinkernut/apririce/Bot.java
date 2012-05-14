108499f18f7948e254b2eec386a7dc58a34483c9
Elazar55 <elazar55@hotmail.com>
2012-05-14 17:39:13 +0100

    Damn squiqlies so annoying. Happy?

--- src/org/tinkernut/apririce/Bot.java
+++ src/org/tinkernut/apririce/Bot.java
@@ -1,12 +1,8 @@
 package org.tinkernut.apririce;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
