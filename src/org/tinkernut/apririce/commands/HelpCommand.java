9e7bc6df3113e37b2a64c421d5b1a21727682056
Elazar55 <elazar55@hotmail.com>
2012-05-14 17:36:25 +0100

    Help, but more official-like.

--- src/org/tinkernut/apririce/commands/HelpCommand.java
+++ src/org/tinkernut/apririce/commands/HelpCommand.java
@@ -3,8 +3,11 @@ package org.tinkernut.apririce.commands;
 import org.tinkernut.apririce.textUtils.TextBuffer;
 
 public class HelpCommand extends Command {
+	@SuppressWarnings(value = { "unused" })
+	private String helpText = "I'm sorry Dave. I'm afraid I can't let you do that.";
+	
 	public void exec() {
-		TextBuffer.addAndDisplay("I'm sorry Dave. I'm afraid I can't let you do that.", me);
+		TextBuffer.addAndDisplay(getHelpText(), me);
 	}
 
 	public void execPriv() {
